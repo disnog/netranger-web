7fdf777fcfa41f2d644fd0d9771eb9cc8c03ff8a
Jason Rokeach <1076569+jrokeach@users.noreply.github.com>
2020-11-06 17:54:49 -0500

    Join form works.

--- nrweb/nrdb.py
+++ nrweb/nrdb.py
@@ -15,15 +15,19 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from flask import g
+from datetime import datetime
+from pymongo import ReturnDocument
 
 
 def get_guild(guild_id):
     return g.db.db.guilds.find_one({"_id": guild_id})
 
+
 def get_user(user_id):
     q = {"_id": int(user_id)}
     r = g.db.db.users.find_one(q)
     return r
+
 
 def get_role_by_significance(guild_id, role_significance):
     q = {"_id": guild_id, "known_roles.significance": role_significance}
@@ -36,26 +40,49 @@ def get_channel_by_significance(guild_id, channel_significance):
     r = g.db.db.guilds.find_one(q, {"known_channels.$": 1})
     return r["known_channels"][0]
 
-# TODO: Fix these! They break schema.
-# def add_channel_significance(channel_id, guild_id, channel_significance):
-#     q = {"_id": id, "guild_id": guild_id}
-#     update = {
-#         "$addToSet": {
-#             "known_channels.id": channel_id,
-#             f"known_channels.{channel_id}.significance": channel_significance,
-#         }
-#     }
-#     r = g.db.db.guilds.updateOne(q, update, {"upsert": True})
-#     return r
-#
-#
-# def add_role_significance(role_id, guild_id, role_significance):
-#     q = {"_id": id, "guild_id": guild_id}
-#     update = {
-#         "$addToSet": {
-#             "known_roles.id": role_id,
-#             f"known_roles.{role_id}.significance": role_significance,
-#         }
-#     }
-#     r = g.db.db.guilds.updateOne(q, update, {"upsert": True})
-#     return r
+
+def upsert_member(user, role_significances):
+    for role_significance in role_significances:
+        if role_significance not in user['permanent_roles']:
+            user['permanent_roles'].append(role_significance)
+    m = {
+        "name": user['username'],
+        "discriminator": user['discriminator'],
+        "permanent_roles": user['permanent_roles'],
+    }
+    if not (user.get('first_joined_at')):
+        m.update({"first_joined_at": datetime.utcnow().timestamp()})
+    if not (user.get('member_number')):
+        nextnumber = g.db.db.config.find_one_and_update(
+            {"name": "last_member_number"},
+            {"$inc": {"value": 1}},
+            projection={"value": True, "_id": False},
+            return_document=ReturnDocument.AFTER,
+            upsert=True,
+        )["value"]
+        m.update({"member_number": nextnumber})
+    g.db.db.users.update_one({"_id": int(user['id'])}, {"$setOnInsert": {"_id": int(user['id']) }, "$set": m}, upsert=True)
+
+    # TODO: Fix these! They break schema.
+    # def add_channel_significance(channel_id, guild_id, channel_significance):
+    #     q = {"_id": id, "guild_id": guild_id}
+    #     update = {
+    #         "$addToSet": {
+    #             "known_channels.id": channel_id,
+    #             f"known_channels.{channel_id}.significance": channel_significance,
+    #         }
+    #     }
+    #     r = g.db.db.guilds.updateOne(q, update, {"upsert": True})
+    #     return r
+    #
+    #
+    # def add_role_significance(role_id, guild_id, role_significance):
+    #     q = {"_id": id, "guild_id": guild_id}
+    #     update = {
+    #         "$addToSet": {
+    #             "known_roles.id": role_id,
+    #             f"known_roles.{role_id}.significance": role_significance,
+    #         }
+    #     }
+    #     r = g.db.db.guilds.updateOne(q, update, {"upsert": True})
+    #     return r
